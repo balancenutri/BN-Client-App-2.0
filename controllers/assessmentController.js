d7eeb83c5718126ae4b5d67f774b6c3cbf4c39ad
VAISH-16 <salunkhevaishnavi2001@gmail.com>
2024-05-24 16:29:06 +0530

    personalDetail

--- controllers/assessmentController.js
+++ controllers/assessmentController.js
@@ -43,3 +43,78 @@ export const assessmentList = async (req, res) => {
     };
   return res.status(200).json(responseData);
 }
+
+export const personalDetails = async (req, res) => {
+    let responseData;
+    responseData = {
+        
+          status: true,
+          message: "personal details",
+          assessment_id: "22485",
+          form_data: [
+                {
+                  id: "53",
+                  asid: "22485",
+                  user_id: "27379",
+                  name: " ",
+                  email_id: "",
+                  mobile_no: "",
+                  address: "",
+                  country: "",
+                  state: "",
+                  city: "",
+                  birth_date: "",
+                  gender: "",
+                  caste: "",
+                  age: "",
+                  asses_photo: "",
+                  weight: "",
+                  height: "",
+                  goal_weight: "",
+                  other_goal: "",
+                  school_time: "",
+                  recess_time: "",
+                  school_days: "",
+                  martial_status: "",
+                  family: "",
+                  no_children: "",
+                  breast_feed: "",
+                  excl_breastfeed: "",
+                  age_child1: "",
+                  age_child2: "",
+                  age_child3: "",
+                  ass_attachment: "",
+                  is_correct: "0",
+                  created: "",
+                  edit_right: "0",
+                  medi: "",
+                  ass_mail: "0000-00-00 00:00:00",
+                  change_url: "",
+                  age_child4: "",
+                  age_child5: "",
+                  child_status: "",
+                  other_caste: "",
+                  breast_feed_freq: "",
+                  breast_feed_other_freq: "",
+                  breast_feed_food: "",
+                  body_type: "",
+                  is_pregnant: "",
+                  pregnancy_month: "",
+                  anniversary_date: "",
+                  dummy: "",
+                  country_name: "",
+                  state_name: "",
+                  city_name: "",
+                  weight_in_kg: "",
+                  weight_in_gm: "0",
+                  goal_weight_in_kg: "",
+                  goal_weight_in_gm: "0",
+                  height_in_feet: "",
+                  height_in_inch: "0",
+                  goal_weight_start: 40
+                }
+            ]
+        
+    };
+  return res.status(200).json(responseData);
+}
--- routes/assessmentRoutes.js
+++ routes/assessmentRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { assessmentList } from "../controllers/assessmentController.js";
+import { assessmentList,personalDetails } from "../controllers/assessmentController.js";
 const assessmentRouter = express.Router();
 
 assessmentRouter.post('/assessmentList', (req, res) => {
     assessmentList(req, res);
 });
+
+assessmentRouter.post('/personalDetails', (req, res) => {
+    personalDetails(req, res);
+});
+
 export default assessmentRouter;
