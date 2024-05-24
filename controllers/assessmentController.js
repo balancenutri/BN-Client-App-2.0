0f53306391699503db74f8315b49c65ade96dfe1
VAISH-16 <salunkhevaishnavi2001@gmail.com>
2024-05-24 16:08:50 +0530

    assessment2

--- controllers/assessmentController.js
+++ controllers/assessmentController.js
@@ -2,6 +2,44 @@ export const assessmentList = async (req, res) => {
     let responseData;
     responseData = {
         
+          status: true,
+          message: "assessment list",
+          assessment_id: "22485",
+          client_number: "8928001617",
+          last_visited_screen: "assessment/1",
+          assessment_status: 0,
+          medical_issue: "0",
+          data: [
+                {
+                  name: "Personal Details",
+                  status: "2"
+                },
+                {
+                  name: "Nutrition & Lifestyle Analysis",
+                  status: "2"
+                },
+                {
+                  name: "24 Hour Diet Recall",
+                  status: "1"
+                },
+                {
+                  name: "Food Frequency",
+                  status: "2"
+                },
+                {
+                  name: "Upload Photo",
+                  status: "2"
+                },
+                {
+                  name: "Medical History",
+                  status: "2"
+                },
+                {
+                  name: "Note To Mentor & Khyati",
+                  status: "2"
+                }
+            ]
+        
     };
   return res.status(200).json(responseData);
 }
